c4265124a8e29e352e0529c4ec216390cf1ea4ff
StudyingWeb <90613276+StudyingWeb@users.noreply.github.com>
2021-09-14 23:31:08 +0300

    Add files via upload

--- mainpage-script.js
+++ mainpage-script.js
@@ -1,31 +1,33 @@
 let p=document.querySelector(":root");
 let nav=document.querySelector("header");
+let logo_nav=document.querySelector(".logo-nav");
 
 let logo = document.querySelector(".logo");
 
 let wdt=document.documentElement.clientWidth;
 let hgt=window.innerHeight;
+logo_nav.style.fontSize=0.035*hgt;
 
 window.addEventListener('scroll', ()=> 
     {
         if (window.scrollY>hgt-70)
         {
-            nav.style.background="rgba(0,0,0,0.1)";
-            logo.style.transition="all 0.1s ease";
+            nav.style.background="blue";
         }
         else
         {
             nav.style.background="";
-            logo.style.transition="all 0.4s ease";
         }
 
         if (window.scrollY>50)
         {
             logo.classList.add("calm");
+            logo_nav.classList.add("calm");
         }
         else
         {
             logo.classList.remove("calm");  
+            logo_nav.classList.remove("calm");  
         }
     }
 );
